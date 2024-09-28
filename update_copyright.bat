0580a34289ce7cc89a72168644348d2c439663ee
Jerry Zhu <xpost2000cod@gmail.com>
2024-09-28 16:54:09 -0400

    copyright updater, custom program not included in source tree.

---
+++ update_copyright.bat
@@ -0,0 +1,2 @@
+@rem this script depends on a private-tool, feel free to ignore this script.
+copyright --holder "Xpost2000" --project "SOULSTORM" --template copyright-template.txt --start-year 2023 Bigfilepacker/*.cpp Bigfilepacker/*.h src/* src/stages/* stages/*
