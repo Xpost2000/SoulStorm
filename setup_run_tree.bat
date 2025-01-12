21cbcd3cbef1d3bd988218e6634d0d6ec3015e30
Jerry Zhu <xpost2000cod@gmail.com>
2025-01-11 22:07:23 -0500

    edit run-tree script

--- setup_run_tree.bat
+++ setup_run_tree.bat
@@ -1,6 +1,6 @@
 @rem run previous script targets first, like the bigfile packer.
 @rem this script does not check for the existence of some files.
-Bigfilepacker.exe data.bigfile res/ stages/
+@call "build_distribution_bundle.bat"
 mkdir run-tree
 copy data.bigfile run-tree
 copy game.exe run-tree
