59567c11d2497f76868c548ced3fcabd48116953
Jerry Zhu <xpost2000cod@gmail.com>
2025-02-01 21:00:34 -0500

    2-2 pushed to 1:10, need about one more minute

--- stages/2_2.lua
+++ stages/2_2.lua
@@ -116,6 +116,8 @@ function Make_Enemy_Helix_Turret_2_2(
    bullet_visual, bullet_visual1
 )
 local e = enemy_new();
+-- todo
+return e;
 end
 
 function wave2()
@@ -154,6 +156,100 @@ function wave2()
       enemy_set_visual(e, ENTITY_SPRITE_BAT_B);
    end
    t_wait(3)
+   for i=1,6 do
+      local e = Make_Enemy_Spinner_1_1_2(
+         15, 
+         v2(-15 - i*15, 20 + i * 45),
+         v2(1, 0),
+         120,
+         1.0,
+         0.0,
+         50,
+         5,
+         5,
+         PROJECTILE_SPRITE_NEGATIVE_DISK,
+         4
+         );
+      enemy_set_visual(e, ENTITY_SPRITE_BAT_B);
+   end
+   for i=1,6 do
+      local e = Make_Enemy_Spinner_1_1_2(
+         15, 
+         v2(play_area_width() + i*15, 20 + i * 45),
+         v2(-1, 0),
+         120,
+         1.0,
+         0.0,
+         50,
+         5,
+         5,
+         PROJECTILE_SPRITE_NEGATIVE_DISK,
+         4
+         );
+      enemy_set_visual(e, ENTITY_SPRITE_BAT_B);
+   end
+   t_wait(2);
+   do
+      local e0 = Make_Enemy_SideMoverWave1_1_1(100, -100, 0, 200, 1.5, 1, 0, 8, PROJECTILE_SPRITE_RED_ELECTRIC, PROJECTILE_SPRITE_RED_DISK);
+      enemy_set_visual(e0, ENTITY_SPRITE_SKULL_B);
+      local e1 = Make_Enemy_SideMoverWave1_1_1(play_area_width() - 100, -100, 0, 200, 1.5, 1, 0, 4, PROJECTILE_SPRITE_GREEN_ELECTRIC, PROJECTILE_SPRITE_GREEN_DISK);
+      enemy_set_visual(e1, ENTITY_SPRITE_SKULL_B);
+      local e2 = Make_Enemy_SideMoverWave1_1_1(150, -100, 0, 200, 1.0, 1, 0, 8, PROJECTILE_SPRITE_RED_ELECTRIC, PROJECTILE_SPRITE_RED_DISK);
+      enemy_set_visual(e2, ENTITY_SPRITE_SKULL_B);
+      local e3 = Make_Enemy_SideMoverWave1_1_1(play_area_width() - 150, -100, 0, 200, 1.0, 1, 0, 4, PROJECTILE_SPRITE_GREEN_ELECTRIC, PROJECTILE_SPRITE_GREEN_DISK);
+      enemy_set_visual(e3, ENTITY_SPRITE_SKULL_B);
+   end
+   t_wait(7);
+   -- start using new enemy sprites here
+   convert_all_bullets_to_score();
+   do
+      local e2 = Make_Enemy_SideMoverWave1_1_1(150, -100, 0, 200, 1.0, 1, 0, 8, PROJECTILE_SPRITE_RED_ELECTRIC, PROJECTILE_SPRITE_RED_DISK);
+      enemy_set_visual(e2, ENTITY_SPRITE_SKULL_B);
+      local e3 = Make_Enemy_SideMoverWave1_1_1(play_area_width() - 150, -100, 0, 200, 1.0, 1, 0, 4, PROJECTILE_SPRITE_GREEN_ELECTRIC, PROJECTILE_SPRITE_GREEN_DISK);
+      enemy_set_visual(e3, ENTITY_SPRITE_SKULL_B);
+   end
+   t_wait(2.5);
+   LaserChaser_Horizontal_1_2(4, 1.25);
+   for i=1,3 do
+      local e = Make_Enemy_Spinner_1_1_2(
+         15, 
+         v2(-15 - i*15, 20 + i * 45),
+         v2(1, 0),
+         120,
+         1.0,
+         0.0,
+         50,
+         5,
+         5,
+         PROJECTILE_SPRITE_RED_DISK,
+         4
+         );
+      enemy_set_visual(e, ENTITY_SPRITE_BAT_B);
+   end
+   for i=1,3 do
+      local e = Make_Enemy_Spinner_1_1_2(
+         15, 
+         v2(play_area_width() + i*15, 20 + i * 45),
+         v2(-1, 0),
+         120,
+         1.0,
+         0.0,
+         50,
+         5,
+         5,
+         PROJECTILE_SPRITE_RED_DISK,
+         4
+         );
+      enemy_set_visual(e, ENTITY_SPRITE_BAT_B);
+   end
+   do
+      local e0 = Make_Enemy_SideMoverWave1_1_1(100, -100, 0, 200, 1.5, 1, 0, 8, PROJECTILE_SPRITE_RED_ELECTRIC, PROJECTILE_SPRITE_RED_DISK);
+      enemy_set_visual(e0, ENTITY_SPRITE_SKULL_B);
+      local e1 = Make_Enemy_SideMoverWave1_1_1(play_area_width() - 100, -100, 0, 200, 1.5, 1, 0, 4, PROJECTILE_SPRITE_GREEN_ELECTRIC, PROJECTILE_SPRITE_GREEN_DISK);
+      enemy_set_visual(e1, ENTITY_SPRITE_SKULL_B);
+   end
+   t_wait(2.5);
+   LaserChaser_Vertical_1_2(4, 1.25);
 end
 
 function stage_task()
@@ -179,7 +275,7 @@ function stage_task()
    t_wait(2);
    wave1();
    wave2();
-
+   t_wait(3);
    wait_no_danger();
    t_complete_stage();
 end
