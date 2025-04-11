dcca3787f25b5cb65b75ee60627b9ec310bd5267
Jerry Zhu <xpost2000cod@gmail.com>
2025-04-10 22:47:09 -0400

    3_1 and 3_2 backgrounds
    More asset flipping, since honestly, they look pretty nice and I don't
    have as much art time to do more dedicated sprites anymore...
    
    I'm basically trying to get as milage as possible out of whatever
    sprites I do have.

--- stages/3_1.lua
+++ stages/3_1.lua
@@ -1,6 +1,12 @@
 engine_dofile("stages/common.lua")
 
 function stage_task()
+   Generic_Infinite_Stage_ScrollV_BG("res/img/stagebkg/stage3bkg1_0.png", 0.28, 0, 0);
+
+   Generic_Infinite_Stage_ScrollV_BG("res/img/stagebkg/stage1bkg1_star0.png", 0.487, 225, 85);
+   Generic_Infinite_Stage_ScrollV_BG("res/img/stagebkg/stage1bkg1_star1.png", 0.287, -120, 175);
+   Generic_Infinite_Stage_ScrollV_FG("res/img/stagebkg/stage1bkg1_star0.png", 0.187, 355, 15);
+   Generic_Infinite_Stage_ScrollV_FG("res/img/stagebkg/stage1bkg1_star1.png", 0.117, -150, 255);
    wait_no_danger();
    t_complete_stage();
 end
--- stages/3_2.lua
+++ stages/3_2.lua
@@ -1,6 +1,16 @@
 engine_dofile("stages/common.lua")
 
 function stage_task()
+   Generic_Infinite_Stage_ScrollV_BG("res/img/stagebkg/stage3bkg2_0.png", 0.50, 0, 0);
+
+   Generic_Infinite_Stage_ScrollV_BG("res/img/stagebkg/stage1bkg1_star0.png", 0.117, 225, 125);
+   Generic_Infinite_Stage_ScrollV_BG("res/img/stagebkg/stage1bkg1_star1.png", 0.147, -150, 245);
+
+   Generic_Infinite_Stage_ScrollV_FG("res/img/stagebkg/stage1bkg1_star0.png", 0.27, 100, 65);
+   Generic_Infinite_Stage_ScrollV_FG("res/img/stagebkg/stage1bkg1_star1.png", 0.37, 500, 200);
+
+   Generic_Infinite_Stage_ScrollV_BG("res/img/stagebkg/stage2bkg2_1.png", 0.180, 0, 125);
+   Generic_Infinite_Stage_ScrollV_BG("res/img/stagebkg/stage1bkg2_1_2.png", 0.150, 0, 305);
    wait_no_danger();
    t_complete_stage();
 end
